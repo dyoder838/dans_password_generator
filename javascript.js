ee50b62d2575c4f7132cba7c194820543c54207c
dyoder838 <70074229+dyoder838@users.noreply.github.com>
2020-09-09 23:38:46 -0700

    coded variables in .js, added function to button click in html

---
+++ javascript.js
@@ -0,0 +1,36 @@
+
+let passwordLength= prompt("How long would you like your password to be? type a number between 8 and 128 to set character length")
+let passwordLower= confirm("Does your password need to have lowercase letters? ok= yes, cancel= no")
+let passwordUpper= confirm("does your password need to contain uppercase letters? ok= yes, cancel= no")
+let passwordNumbers= confirm("does your password need to contain numbers? ok= yes, cancel= no")
+let passwordCharacter= confirm("does your password need to contain special characters? ( !#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ) ok= yes, cancel= no")
+
+//  verification code 
+//  if (confirm("Press a button!")) {
+//   txt = "You pressed OK!";
+// } else {
+//   txt = "You pressed Cancel!";
+// }
+// generate random password
+function generate() {
+
+
+    // initial pro
+
+
+    // set password length / complexity
+    // let complexity = document.getElementsByTagName("").value
+    // complexity will be set by 5 variables
+    function howComplex(low, up, num, char){
+
+    };
+
+    // use these to store user data for conditions
+    let complexLowercase = "abcdefghijklmnopqrstuvwxyz";
+    let complexUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let complexNumbers = "1234567890";
+    let complexCharacter = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    let password= "";
+    // for loop to chose characters
+
+};
